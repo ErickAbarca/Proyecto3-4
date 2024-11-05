a4041e1950a207ca7e55ea61da586cf566f1a725
chuz0 <115124922+chuz0@users.noreply.github.com>
2024-11-04 18:18:01 -0600

    archivo app.py creado

---
+++ app.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from flask import Flask, jsonify, redirect, render_template, request, url_for
+import pyodbc
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+# Configuración de la conexión a la base de datos
+def get_db_connection():
+    connection = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'SERVER=ERICKPC;'
+        'DATABASE=proyecto2;'
+        'UID=hola;'
+        'PWD=12345678'
+    )
+    return connection
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
